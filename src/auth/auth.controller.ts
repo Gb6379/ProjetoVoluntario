0230a854f83a75edc282db908c046705ab49475b
Gb6379 <gabriel.am.rosa@gmail.com>
2024-05-04 00:45:38 -0300

    login endpoint and custom decorators

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Req } from '@nestjs/common';
+import { ApiOkResponse } from '@nestjs/swagger';
+import { UserToken } from 'src/responses/UserTokenResponse';
+import { AuthService } from './auth.service';
+import { Required } from 'src/custom-decorators/required.decorator';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+
+    constructor(private authService: AuthService) {}
+
+    //@ApiOkResponse(USER_TOKEN_RESPONSE)
+    @Post('login')
+    async login(@Required('email') email: string, @Required('password') password: string): Promise<UserToken> {
+      return this.authService.login(email, password);
+    }
+
+    // async register():Promise<null> {
+        
+    // }
+  
+  
+}
